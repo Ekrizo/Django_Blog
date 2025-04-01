3eedab890e5f87e8039c1dfed0c035b1983efcca
James Roxas <roxasjamesmartin@gmail.com>
2025-04-01 18:49:30 +0800

    Added Gunicorn to requirements

--- projec1/projec1/urls.py
+++ projec1/projec1/urls.py
@@ -1,31 +1,18 @@
-"""
-URL configuration for projec1 project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# projec1/urls.py
 from django.contrib import admin
 from django.urls import include, path
 from django.contrib.sitemaps.views import sitemap
 from blog.sitemaps import PostSitemap
+from blog import views  # Import the blog views
 
 sitemaps = {
-    'posts': PostSitemap, 
+    'posts': PostSitemap,
 }
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', include('blog.urls', namespace='blog')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, 
-         name='django.contrib.sitemaps.views.sitemaps')
+         name='django.contrib.sitemaps.views.sitemaps'),
+    path('', views.blog_home, name='home'),  # Set blog homepage as root
 ]
